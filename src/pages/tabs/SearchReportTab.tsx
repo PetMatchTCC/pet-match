e7e66f25201f3e56fb00e5973380f9640adad452
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2025-04-10 15:55:55 -0300

    refac: change report seach form to react-hook-form standart

--- src/pages/tabs/SearchReportTab.tsx
+++ src/pages/tabs/SearchReportTab.tsx
@@ -1,34 +1,79 @@
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { Search } from "lucide-react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface FormInput {
+  reportId: string;
+}
 
 const SearchReportTab: React.FC = () => {
+  const form = useForm<FormInput>({ defaultValues: { reportId: "" } });
 
-  const [reportId, setReportId] = useState<string>("");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setReportId(e.target.value);
-  };
-
-  const handleSearch = () => {
-    console.log(reportId);
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    console.log(data.reportId);
   };
 
   return (
-    <div className="gap-4 items-center flex flex-col justify-center w-full mb-8">
-      <h3 className="text-xl font-medium my-2">Buscar denúncia por código</h3>
-      <div className="w-full max-w-md">
-        <Input
-          id="id_denuncia"
-          placeholder="Digite o código da denúncia"
-          className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-          onChange={handleChange}
-          value={reportId}
-        />
-        <Button className="my-2 self-end w-full bg-orange-500 hover:bg-orange-600" onClick={handleSearch}>Buscar</Button>
+    <>
+      <div className="flex flex-row items-center justify-center gap-2">
+        <Search />
+        <h3 className="text-xl font-medium my-3 text-center">
+          Buscar denúncia por código
+        </h3>
       </div>
-    </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full flex justify-center"
+        >
+          <div className="space-y-6 w-full max-w-md mb-4">
+            <FormField
+              control={form.control}
+              name="reportId"
+              rules={{
+                required: "Não é possível buscar um código em branco.",
+                minLength: {
+                  value: 20,
+                  message:
+                    "Por padrão, o código de uma denúncia deve ter 20 caractéres.",
+                },
+              }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="topic">Código da denúncia</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="reportId"
+                      type="text"
+                      placeholder="Insira aqui o código da denúncia"
+                      className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-500 text-sm" />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="w-full my-8 p-3 bg-orange-400 text-white rounded-lg hover:bg-orange-700 focus:ring-4 focus:ring-blue-300"
+            >
+              Buscar
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
   );
-}
+};
 
 export default SearchReportTab;
