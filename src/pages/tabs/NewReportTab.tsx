a061ebfb6761c86ff74e5191a60d71977341adb5
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2025-04-10 16:04:17 -0300

    feat: new report form validation

--- src/pages/tabs/NewReportTab.tsx
+++ src/pages/tabs/NewReportTab.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage, Form } from "@/components/ui/form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { SelectTrigger, Select, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+import {
+  SelectTrigger,
+  Select,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
 import { db } from "@/firebase/fireConfig";
 import { push, ref, set } from "firebase/database";
-import { FormInput } from "lucide-react";
+import { MessageSquareWarning } from "lucide-react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface FormInput {
@@ -18,58 +31,93 @@ const NewReportTab = () => {
     defaultValues: {
       topic: "",
       title: "",
-      description: ""
+      description: "",
     },
-  })
+  });
 
   const handleInsertReport = async (data: FormInput): Promise<void> => {
     try {
-
-      const dbRef = ref(db, 'report');
+      const dbRef = ref(db, "report");
       const newReportRef = push(dbRef);
 
       await set(newReportRef, data);
 
       await navigator.clipboard.writeText(newReportRef.key);
 
-      console.log('Relatório registrado com sucesso! ID:', newReportRef.key);
+      console.log(
+        "Relatório registrado com sucesso! \n Código: ",
+        newReportRef.key
+      );
 
-      alert(`Denúncia enviada com sucesso! Confira o estado da denúncia através da busca por ID: ${newReportRef.key}`)
-    }
-    catch (err) {
+      alert(
+        `Denúncia enviada com sucesso! Confira o estado da denúncia através da busca por ID: ${newReportRef.key}`
+      );
+    } catch (err) {
       alert(`Erro: ${err}`);
     }
-
-  }
+  };
 
   const onSubmit: SubmitHandler<FormInput> = (data) => {
-    handleInsertReport(data)
-  }
+    handleInsertReport(data);
+  };
 
   return (
     <>
-      <h3 className="text-xl font-medium my-3 text-center">Registrar nova denúncia</h3>
+      <div className="flex flex-row items-center justify-center gap-2">
+        <MessageSquareWarning />
+        <h3 className="text-xl font-medium my-3 text-center">
+          Registrar nova denúncia
+        </h3>
+      </div>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full flex justify-center">
+          className="w-full flex justify-center"
+        >
           <div className="space-y-6 w-full max-w-md">
             <FormField
               control={form.control}
               name="topic"
+              rules={{
+                required: "Escolha um tópico para classificar sua denúncia. ",
+              }}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel htmlFor="topic">Tópico</FormLabel>
+                  <FormLabel htmlFor="topic">Tópico*</FormLabel>
                   <FormControl>
-                    <Select value={field.value} onValueChange={field.onChange} disabled={field.disabled} >
-                      <SelectTrigger className="w-full" >
+                    <Select
+                      value={field.value}
+                      onValueChange={field.onChange}
+                      disabled={field.disabled}
+                    >
+                      <SelectTrigger className="w-full">
                         <SelectValue placeholder="Selecione o tipo de denúncia" />
                       </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="bad">Maus-tratos</SelectItem>
-                        <SelectItem value="ilegal">Venda ilegal</SelectItem>
-                        <SelectItem value="neglect">Negligência</SelectItem>
-                        <SelectItem value="other">Outros</SelectItem>
+                      <SelectContent className="hover:cursor-pointer">
+                        <SelectItem
+                          value="bad"
+                          className="hover:cursor-pointer"
+                        >
+                          Maus-tratos
+                        </SelectItem>
+                        <SelectItem
+                          value="ilegal"
+                          className="hover:cursor-pointer"
+                        >
+                          Venda ilegal
+                        </SelectItem>
+                        <SelectItem
+                          value="neglect"
+                          className="hover:cursor-pointer"
+                        >
+                          Negligência
+                        </SelectItem>
+                        <SelectItem
+                          value="other"
+                          className="hover:cursor-pointer"
+                        >
+                          Outros
+                        </SelectItem>
                       </SelectContent>
                     </Select>
                   </FormControl>
@@ -80,9 +128,16 @@ const NewReportTab = () => {
             <FormField
               control={form.control}
               name="title"
+              rules={{
+                required: "É obrigatório escolher um título.",
+                minLength: {
+                  value: 5,
+                  message: "O título é muito curto.",
+                },
+              }}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel htmlFor="title">Título da denúncia</FormLabel>
+                  <FormLabel htmlFor="title">Título da denúncia*</FormLabel>
                   <FormControl>
                     <Input
                       id="title"
@@ -99,12 +154,21 @@ const NewReportTab = () => {
             <FormField
               control={form.control}
               name="description"
+              rules={{
+                required: "É obrigatório descrever sua denúncia.",
+                minLength: {
+                  value: 50,
+                  message: "A descrição é muito curta.",
+                },
+              }}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel htmlFor="description">Descreva a situação</FormLabel>
+                  <FormLabel htmlFor="description">
+                    Descreva a situação*
+                  </FormLabel>
                   <FormControl>
                     <textarea
-                      id="title"
+                      id="description"
                       placeholder="Escreva os detalhes da sua denúncia aqui"
                       className="w-full h-32 text-base leading-6 resize-y p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
                       {...field}
@@ -117,12 +181,14 @@ const NewReportTab = () => {
             <Button
               type="submit"
               className="w-full my-8 p-3 bg-orange-400 text-white rounded-lg hover:bg-orange-700 focus:ring-4 focus:ring-blue-300"
-            >Enviar</Button>
+            >
+              Registrar
+            </Button>
           </div>
         </form>
       </Form>
     </>
   );
-}
+};
 
 export default NewReportTab;
